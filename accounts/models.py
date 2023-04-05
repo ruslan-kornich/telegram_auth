2e261b207887cf28ee09abcbd8a21b0064b08304
ruslan-kornich <korny4rus143@gmail.com>
2023-04-05 20:43:09 +0300

    models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    telegram_id = models.IntegerField(unique=True)
+    username = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    photo = models.ImageField(blank=True, null=True, upload_to="media/photos")
+
+    def __str__(self):
+        return self.username
